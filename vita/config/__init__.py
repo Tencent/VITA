0166306a40cc824e7c2d779592eb84b9c69f14c1
Yunhang Shen <shenyunhang01@gmail.com>
2024-12-12 11:10:04 +0800

    clean history

---
+++ vita/config/__init__.py
@@ -0,0 +1,10 @@
+from .dataset_config import *
+
+NaturalCap0 = [ShareGPT4V0]
+NaturalCap = [ShareGPT4V]
+
+DataConfig = {
+    "Pretrain_video": NaturalCap0,
+}
+
+NoPatchSets = ["khair", "jester"]
---
+++ vita/config/dataset_config.py
@@ -0,0 +1,8 @@
+AudioFolder = ""
+FolderDict = {
+    #### NaturalCap
+    "sharegpt4": "",
+}
+#### NaturalCap
+ShareGPT4V = {"chat_path": ""}
+ShareGPT4V0 = {"chat_path": ""}
